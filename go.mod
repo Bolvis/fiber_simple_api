5635a929e86b30ba6bd94f394b6d45a0dead203b
Dawid <dawid.stasiak17@gmail.com>
2021-09-02 09:05:23 +0200

    error handling

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module fiber_tutorial
 
 go 1.16
 
-require github.com/gofiber/fiber/v2 v2.18.0 // indirect
+require (
+	github.com/gofiber/fiber/v2 v2.18.0
+	github.com/ramya-rao-a/go-outline v0.0.0-20210608161538-9736a4bde949 // indirect
+)
--- person/person.go
+++ person/person.go
@@ -13,6 +13,11 @@ type Person struct {
 
 func GetAllPeople(c *fiber.Ctx) error {
 	array_of_people := CreateArrayOfPeople()
-	response, _ := json.Marshal(array_of_people)
+	response, err := json.Marshal(array_of_people)
+
+	if err != nil {
+		return c.Status(500).SendString("Internal server error :(")
+	}
+
 	return c.Send(response)
 }
